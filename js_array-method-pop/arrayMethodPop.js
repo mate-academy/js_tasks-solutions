b05ffaa320d660aa640f2aed33d9bf856578786d
gestap <gestap9@gmail.com>
2019-04-19 17:43:39 +0300

    Change solution methodPush, use for of loop instead for. Change variable name "element to lastElement"

--- js_array-method-pop/arrayMethodPop.js
+++ js_array-method-pop/arrayMethodPop.js
@@ -7,9 +7,9 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function () {
     if (this.length > 0) {
-      let element = this[this.length - 1];
+      let lastElement = this[this.length - 1];
       this.length--;
-      return element;
+      return lastElement;
     }
   };
 }
