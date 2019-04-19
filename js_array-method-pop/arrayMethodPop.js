22cd3dfdb54b4f0e160f45acfe3bbd597be93fa3
gestap <gestap9@gmail.com>
2019-04-19 17:10:30 +0300

    Add solutions for arrayPush and arrayPop tasks

---
+++ js_array-method-pop/arrayMethodPop.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Implement method pop
+ *
+ */
+function applyCustomPop() {
+  [].__proto__.pop2 = function () {
+    if (this.length > 0) {
+      let element = this[this.length - 1];
+      this.length--;
+      return element;
+    }
+  };
+}
+
+module.exports = applyCustomPop;
