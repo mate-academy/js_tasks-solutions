2548e1c1642d33155d9595397bcb2326133e3222
gestap <gestap9@gmail.com>
2019-04-22 16:01:34 +0300

    Updated join solution

--- js_array-method-join/arrayMethodJoin.js
+++ js_array-method-join/arrayMethodJoin.js
@@ -5,9 +5,9 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
+    // Converting to string to support null and object as separators.
     let separatorStr = String(separator);
-    let res = '';
-    res += (this[0] === undefined || this[0] === null ? '' : this[0]);
+    let res = '' + (this[0] === undefined || this[0] === null ? '' : this[0]);
     for (let i = 1; i <= this.length - 1; i++) {
       let element = (this[i] === undefined || this[i] === null ? '' : this[i]);
       res += separatorStr + element;
