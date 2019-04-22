941717b2594913f1750fafa11439536a910d0e7d
gestap <gestap9@gmail.com>
2019-04-22 15:35:59 +0300

    Updated solution join

--- js_array-method-join/arrayMethodJoin.js
+++ js_array-method-join/arrayMethodJoin.js
@@ -5,11 +5,12 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
+    let separatorStr = String(separator);
     let res = '';
-    res = res + (this[0] === undefined || this[0] === null ? '' : this[0]);
+    res += (this[0] === undefined || this[0] === null ? '' : this[0]);
     for (let i = 1; i <= this.length - 1; i++) {
       let element = (this[i] === undefined || this[i] === null ? '' : this[i]);
-        res = res + separator + element;
+      res += separatorStr + element;
     };
     return res;
   };
