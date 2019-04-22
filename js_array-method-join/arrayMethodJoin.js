b81dc5abd102a1d6a13ea6a6f6b6ea948266df8e
gestap <gestap9@gmail.com>
2019-04-22 13:29:05 +0300

    Update solution for join

--- js_array-method-join/arrayMethodJoin.js
+++ js_array-method-join/arrayMethodJoin.js
@@ -4,18 +4,12 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function (separator = ',') {
+  [].__proto__.join2 = function(separator = ',') {
     let res = '';
-    if (this.length === 0) {
-      return '';
-    };
-    for (let i = 0; i <= this.length - 1; i++) {
+    res = res + (this[0] === undefined || this[0] === null ? '' : this[0]);
+    for (let i = 1; i <= this.length - 1; i++) {
       let element = (this[i] === undefined || this[i] === null ? '' : this[i]);
-      if (i === 0) {
-        res = res + element;
-      } else {
         res = res + separator + element;
-      }
     };
     return res;
   };
